2d84a5c286b39184fb6d1e1aeb3ee0f86caff047
neonradon <neonradon@yahoo.com>
2023-01-17 09:32:58 +0800

    some message

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("easy to use!!");
+        System.out.println("love jenkins!!");
     }
 
     public String getMessage() {
